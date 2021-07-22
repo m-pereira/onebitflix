bfc67c2364883aaef79374ee671dc162da52f06e
Mauricio Lima <mauricio@codebit.com.br>
2021-07-21 22:44:55 -0300

    feat: Add Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_004106) do
+ActiveRecord::Schema.define(version: 2021_07_22_014211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text "content"
