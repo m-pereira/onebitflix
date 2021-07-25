cd7293f5a3b16de3349c87450d87253885ae30c8
Mauricio Lima <mauricio@codebit.com.br>
2021-07-24 23:43:46 -0300

    feat: add Header and Footer components

---
+++ app/javascript/packs/components/shared/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer height="auto" class="black">
+    <v-col class="text-center white--text" cols="12">
+      App Baseado no
+      <a href="https://netflixm" target="_blank" class="white--text">
+        <strong>Netflix.com</strong>
+      </a>
+    </v-col>
+    <v-col class="text-center white--text" cols="12">
+      <p>
+        3º Semana Super Full Stack --
+        <a href="https://onebitcode.com" class="white--text" target="_blank">
+          <strong>OneBitCode.com</strong>
+        </a>
+      </p>
+    </v-col>
+  </v-footer>
+</template>
---
+++ app/javascript/packs/components/shared/header.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-toolbar color="black" dark>
+    <v-layout row wrap>
+      <v-flex xs10 sm4>
+        <img :src="logo" class="logo" />
+      </v-flex>
+    </v-layout>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-text-field
+        text
+        prepend-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+        color="red"
+        pr-3
+      ></v-text-field>
+
+      <v-menu bottom text>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" slot="activator" text>
+            <v-icon large>account_box</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>
+              <a href="/users/edit">Details</a>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <a data-method="delete" href="/users/sign_out">Logout</a>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+import Logo from '../../assets/images/logo.png';
+
+export default {
+  data() {
+    return {
+      logo: Logo,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  width: 120px !important;
+  margin-top: 10px;
+}
+</style>
